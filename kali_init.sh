d0e19090316cda5fc49797ac80e76cfb6b69b77b
Adham Elmosalamy <40863802+aelmosalamy@users.noreply.github.com>
2025-10-14 03:47:48 +0400

    Update kali_init.sh

--- kali_init.sh
+++ kali_init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# functions
 start_step() {
   export step_name="$1"
   echo "[*] ----- $step_name -----"
@@ -11,30 +10,37 @@ end_step() {
   unset step_name
 }
 
-# script
 start_step "updating system"
 apt-get update
 end_step
 
-start_step "populating .zsh_history"
+start_step "getting pentester-worthy .zsh_history"
 curl -s https://raw.githubusercontent.com/Jeremiznoo/Updated_Exegol_Zsh_History/refs/heads/main/.zsh_history_for_kali >> ~/.zsh_history
 end_step
 
-start_step "downloading pipx"
+start_step "installing pipx"
 apt install -y pipx
 pipx ensurepath
 end_step
 
-start_step "getting certipy-ad"
+start_step "installing certipy-ad"
 pipx install certipy-ad
 end_step
 
-start_step "getting bloodyAD"
+start_step "installing bloodyAD"
 pipx install bloodyAD
 end_step
 
-start_step "getting docker"
+start_step "installing docker"
 apt install -y docker.io
 systemctl enable docker --now
 usermod -aG docker $USER
 end_step
+
+start_step "installing bloodhound-cli"
+wget https://github.com/SpecterOps/bloodhound-cli/releases/latest/download/bloodhound-cli-linux-amd64.tar.gz
+tar -xvzf bloodhound-cli-linux-amd64.tar.gz
+./bloodhound-cli install
+end_step
+
+echo "\n------------------------\ndone, enjoy your kali!"
